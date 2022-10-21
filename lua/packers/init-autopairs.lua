8648d2deb117bfe06da919d3f2c5841faf857682
amaomaoa <347317112@qq.com>
2022-10-21 14:58:58 +0800

    ...

--- lua/packers/init-autopairs.lua
+++ lua/packers/init-autopairs.lua
@@ -1,4 +1,4 @@
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = require("cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-local handlers = require("nvim-autopairs.completion.handlers")
+--local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+--local cmp = require("cmp")
+--cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+--local handlers = require("nvim-autopairs.completion.handlers")
